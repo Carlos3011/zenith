e054cb8e1d21fc9cf6b8f02bbb227f72e418005f
Carlos Garcia <91349521+Carlos3011@users.noreply.github.com>
2025-02-21 02:37:23 -0600

    instalacion de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
-use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PublicController::class, 'inicio'])->name('inicio');
-Route::get('/acerca', [PublicController::class, 'acerca'])->name('acerca');
-Route::get('/miembros', [PublicController::class, 'miembros'])->name('miembros');
-Route::get('/chatbots', [PublicController::class, 'chatbots'])->name('chatbots');
-Route::get('/recursos', [PublicController::class, 'recursos'])->name('recursos');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
